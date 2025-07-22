6e8894d692a9ae0fc5c4ef7bd12d755690e66dbb
SyunkiTakase <161103101+SyunkiTakase@users.noreply.github.com>
2025-07-22 16:03:48 +0900

    Add files via upload

---
+++ output/TripletLoss/2025072215/TripletLoss.py
@@ -0,0 +1,40 @@
+
+
+# config = {
+#     "epoch": 20,
+#     "batch_size": 128,
+#     "lr": 1e-3,
+#     "weight_decay": 1e-4,
+#     "momentum": 0.9,
+#     "img_size": 224,
+#     "dataset": "cifar10",
+#     "margin": 1.0,
+#     "scale": None,
+#     "method": "TripletLoss",
+#     "arch": "ResNet18",
+#     "_lambda": 1.0,
+#     "num_dim": None,
+#     "hard_triplets": None,
+#     "easy_margin": None,
+#     "vis_featspace": True
+# }
+
+
+config = {
+    "epoch": 20,
+    "batch_size": 128,
+    "lr": 1e-3,
+    "weight_decay": 1e-4,
+    "momentum": 0.9,
+    "img_size": 224,
+    "dataset": "cifar10",
+    "margin": 1.0,
+    "scale": None,
+    "method": "TripletLoss",
+    "arch": "ResNet18",
+    "_lambda": 0.2,
+    "num_dim": None,
+    "hard_triplets": True,
+    "easy_margin": None,
+    "vis_featspace": True
+}
